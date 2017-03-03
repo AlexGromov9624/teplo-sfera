c6872582166ac2ca0d0f7f7f0410bd3efaf385a5
Daria <ardaua@yandex.ru>
2017-03-03 17:05:31 +0200

    correct

--- application.php
+++ application.php
@@ -3,13 +3,13 @@
 <head>
 <meta charset="UTF-8">
 <title>Создание формы обратной связи</title>
-<meta http-equiv="Refresh" content="4; URL=http://teplo-dnepr.com.ua">
+<meta http-equiv="Refresh" content="4; URL=http://teplo-sfera.com">
 </head>
 <body>
 
 <?php 
 
-$sendto   = "teplosffera@gmail.com"; // почта, на которую будет приходить письмо officeppu@gmail.com
+$sendto   = "teplosffera@gmail.com"; // почта, на которую будет приходить письмо teplosffera@gmail.com
 $username = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
 $usertel = $_POST['telephone']; // сохраняем в переменную данные полученные из поля c телефонным номером
 $usermail = $_POST['email']; // сохраняем в переменную данные полученные из поля c адресом электронной почты
@@ -23,7 +23,7 @@ $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
 
 // Формирование тела письма
 $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта Тепло-днепр</h2>\r\n";
+$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта Тепло-Сфера</h2>\r\n";
 $msg .= "<p><strong>От кого:</strong> ".$username."</p>\r\n";
 $msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
 $msg .= "<p><strong>Телефон:</strong> ".$usertel."</p>\r\n";
