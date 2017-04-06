49f957dee15ccc9a1c7ece8727a7531dc27ffdbc
Daria <ardaua@yandex.ru>
2017-04-06 15:05:46 +0300

    text align just

--- js/fix_menu.js
+++ js/fix_menu.js
@@ -6,13 +6,13 @@ $(document).ready(function(){
     var $menu = $("#menu");
 
     $(window).scroll(function(){
-        if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+        if ( $(this).scrollTop() > 600 && $menu.hasClass("default") ){
             $menu.fadeOut('fast',function(){
                 $(this).removeClass("default")
                     .addClass("fixed transbg")
                     .fadeIn('fast');
             });
-        } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+        } else if($(this).scrollTop() <= 600 && $menu.hasClass("fixed")) {
             $menu.fadeOut('fast',function(){
                 $(this).removeClass("fixed transbg")
                     .addClass("default")
